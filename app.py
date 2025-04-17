9c7a0062bb3cdf91a036c872972948adf98d3512
Geoxing32 <78418474+Geoxing32@users.noreply.github.com>
2025-04-17 16:23:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ def get_listings():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
+    port = int(os.environ.get("PORT", 10000))  # <= CORRECTION ICI
+    app.run(host="0.0.0.0", port=port)
